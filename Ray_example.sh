b3a29211b01bf90869fee4206e1c1be0af0b0cf6
Kerry A. Cobb <kerrycobb@users.noreply.github.com>
2019-06-03 14:04:48 -0500

    Update Ray_example.sh

--- Ray_example.sh
+++ Ray_example.sh
@@ -5,9 +5,9 @@ module load ray/2.3.1
 #### COMMAND LINE FOR RAY GOES BELOW ####
 
 srun --mpi=pmi2 Ray  \
--k 31  \
--p <read-1-fasta-file> <read-2-fasta-file> \
--o <output-file>
+  -k 31  \
+  -p <READ_1_FASTA_PATH> <READ_2_FASTA_PATH> \
+  -o <OUTPUT_PATH>
 
 ####      submit job to class queue as "run_script_mpi this_script_name"         ####
 #### request 10 CPU, 2:00:00 of run time, 20gb of memory and dmc as the cluster  ####

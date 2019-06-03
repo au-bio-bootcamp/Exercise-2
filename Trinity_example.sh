f68d498c95140a910af482b67a1625a5161d5dfc
Kerry A. Cobb <kerrycobb@users.noreply.github.com>
2019-06-03 14:39:53 -0500

    Update Trinity_example.sh

--- Trinity_example.sh
+++ Trinity_example.sh
@@ -3,8 +3,8 @@
 module load trinity/2.2.0
 
 Trinity --seqType fq \
---left  <read-1-fasta-file> \
---right <read-2-fasta-file> \
+--left  <READ-1-FASTA-PATH> \
+--right <READ-2-FASTA-PATH> \
 --CPU 6  --max_memory 20G  --full_cleanup --bflyGCThreads 3 --bflyHeapSpaceMax 20G
 
 ####  submit job to class queue as "run_script this_script_name"  ####
